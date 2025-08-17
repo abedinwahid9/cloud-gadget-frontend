267eaacd67d8127f7d26e631f26c8199273e1678
abedin wahid <abedinwahid9@gmail.com>
2025-08-18 01:07:03 +0600

    apps all color plate setup done

---
+++ components/Navbar/MainNav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainNav = () => {
+  return (
+    <nav>
+      <div className="w-full h-8 bg-primary container mx-auto"></div>
+    </nav>
+  );
+};
+
+export default MainNav;
