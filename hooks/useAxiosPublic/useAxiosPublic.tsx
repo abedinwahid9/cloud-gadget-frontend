25a918a1d3f7be391b7d69b449cda5629b3ae518
abedinwahid9 <abedinwahid9@gmail.com>
2025-10-21 12:50:02 +0600

    loading add skeleton slider ,banner and category table

--- hooks/useAxiosPublic/useAxiosPublic.tsx
+++ hooks/useAxiosPublic/useAxiosPublic.tsx
@@ -1,18 +1,16 @@
 import axios from "axios";
 let BASE_URL;
 
-// if (process.env.NEXT_PUBLIC_ENV === "DEV") {
-//   BASE_URL = process.env.NEXT_PUBLIC_LOCAL;
-// } else {
-//   BASE_URL = process.env.NEXT_PUBLIC_PRODUCTION;
-// }
+if (process.env.NEXT_PUBLIC_ENV === "DEV") {
+  BASE_URL = process.env.NEXT_PUBLIC_LOCAL;
+} else {
+  BASE_URL = process.env.NEXT_PUBLIC_PRODUCTION;
+}
 
-// const axiosPublic = axios.create({
-//   baseURL: BASE_URL,
-// });
 const axiosPublic = axios.create({
-  baseURL: "https://cloud-gadget-server.onrender.com/api/v1",
+  baseURL: BASE_URL,
 });
+
 
 const useAxiosPublic = () => {
   return axiosPublic;
