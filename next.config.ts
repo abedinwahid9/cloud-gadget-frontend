76a5d0f98710de7069c826828f5453179f7c1596
abedinwahid9 <abedinwahid9@gmail.com>
2026-01-10 00:31:00 +0600

    nextjs update issue in remote image, so now fix it work properly

--- next.config.ts
+++ next.config.ts
@@ -3,22 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.pravatar.cc",
-      },
-      {
-        protocol: "https",
-        hostname: "cloud-gadget-server.onrender.com",
-      },
+      new URL("http://localhost:8000/uploads"),
+      new URL("https://i.pravatar.cc"),
+      new URL("https://cloud-gadget-server.onrender.com"),
     ],
     domains: ["localhost", "cloud-gadget-server.onrender.com"],
   },
+
   async rewrites() {
     return [
       {
-        source: "/api/v1/:path*",
-        destination: "https://cloud-gadget-server.onrender.com/api/v1/:path*",
+        source: "/uploads/:path*",
+        destination: "http://localhost:8000/uploads/:path*",
       },
     ];
   },
